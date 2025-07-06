5e90ef02f1813d20d109afc09ff64981b1bc597f
Rohit Khedkar <rohitkhedkar5060@gmail.com>
2025-07-06 18:13:48 +0530

    base URL update

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // for localhost baseURL: "http://localhost:5000/api",
-  baseURL : "https://store-rating-app-7m68.onrender.com/api",
+  baseURL : "https://store-rating-backend-1jhv.onrender.com/api",
   withCredentials: true, 
 });
 
