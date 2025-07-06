f1a51e166519c59a0d449b3d7305b096543323b5
Rohit Khedkar <rohitkhedkar5060@gmail.com>
2025-07-06 16:20:24 +0530

    Initial commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // for localhost baseURL: "http://localhost:5000/api",
+  baseURL : "https://store-rating-backend.vercel.app/api",
   withCredentials: true, 
 });
 
