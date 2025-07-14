a71994a781fd20ab3397e3dbd670ea4852d2be59
Rohit Khedkar <rohitkhedkar5060@gmail.com>
2025-07-14 19:32:18 +0530

    base url add

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  // for localhost baseURL: "http://localhost:5000/api",
-  baseURL : process.env.VITE_BACKEND_URL,
-  withCredentials: true, 
+  baseURL: import.meta.env.VITE_BACKEND_URL, 
+  withCredentials: true,
 });
+
 
 export default api;
